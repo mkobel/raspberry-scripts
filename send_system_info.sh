e387e747100f9b7c83cd57bbd30abe85287c78d8
Moritz Kobel <moritz@kobelnet.ch>
2014-02-11 01:40:20 +0100

    change hostname + submit node name

--- send_system_info.sh
+++ send_system_info.sh
@@ -6,5 +6,5 @@ tc=`echo $tm| cut -d '=' -f2 | sed 's/..$//'`
 vm=`/opt/vc/bin/vcgencmd measure_volts core`
 vv=`echo $vm| cut -d '=' -f2 | sed 's/..$//'`
 
-curl -s -o /dev/null  "http://garden/system?temp="$tc"&voltage="$vv
+curl -s -o /dev/null  "http://webgate-moosseedorf.kobelnet.ch/garden/system?name=$1&temp="$tc"&voltage="$vv
 
